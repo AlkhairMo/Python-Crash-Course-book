063c0913948a092d7acf905a3e02af187bb4573a
Alkhair Mohammed <alkhair.mohammd@gmail.com>
2024-05-22 19:52:01 +0200

    Delete 2.7 Stripping Names.py

--- 2.7 Stripping Names.py
+++
@@ -1,9 +0,0 @@
-name = " Alkhair "
-represent = f"My name is {name}, I am 19 years old"
-print(represent)
-represent = f"My name is {name.rstrip()}, I am 19 years old"
-print(represent)
-represent = f"My name is {name.lstrip()}, I am 19 years old"
-print(represent)
-represent = f"My name is {name.strip()}, I am 19 years old"
-print(represent)
